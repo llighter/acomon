b9f4d8f2173ac04c70c913415ea01879bcb2b99c
llighter <hadbsgk@gmail.com>
2017-04-14 15:56:37 +0900

    관용이형 파일 수정중 : 변수

--- llighter/js/Game.js
+++ llighter/js/Game.js
@@ -1,6 +1,7 @@
 // INIT
 var canvas = document.getElementById("startVillage");
 var context = canvas.getContext("2d");
+
 var map=[
 	[0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0],
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0],
@@ -13,6 +14,16 @@ var map=[
 	[0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1],
 	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ];
+
+const ARROW_LEFT = 37;
+const ARROW_UP = 38;
+const ARROW_RIGHT = 39;
+const ARROW_DOWN = 40;
+
+const EAST_DIRECTION = 0;
+const WEST_DIRECTION = 1;
+const SOUTH_DIRECTION = 2;
+const NORTH_DIRECTION = 3;
 
 var grass=new Image();
 var road01=new Image();
@@ -33,40 +44,40 @@ var charX=0;
 var charY=0;
 
 // Charactor direction
-var charPos = 3;
+var charDirection = EAST_DIRECTION;
 
 function key(){
 	if(event.keyCode==37){
 		if( map[(charY/64)][((charX-64)/64)]==2 || (charX<=0) ){
-			charPos=0;
+			charDirection=0;
 		}else{
 			charX-=64;
-			charPos=0;
+			charDirection=0;
 		}
 	}
 	if(event.keyCode==38){
 		if( map[((charY-64)/64)][(charX/64)]==2 || (charY<=0) ){
-			charPos=1;
+			charDirection=1;
 		}else{
 			charY-=64;
-			charPos=1;
+			charDirection=1;
 		}
 	}
 	if(event.keyCode==39){
 		if( map[(charY/64)][((charX+64)/64)]==2 || (charX>=1216) ){
 			// �������� ����Ʈ(����?)
-			charPos=2;
+			charDirection=2;
 		}else{
 			charX+=64;
-			charPos=2;
+			charDirection=2;
 		}
 	}
 	if(event.keyCode==40){
 		if( (charY>=576) || map[((charY+64)/64)][(charX/64)]==2 ){
-			charPos=3;
+			charDirection=3;
 		}else{
 			charY+=64;
-			charPos=3;
+			charDirection=3;
 		}
 	}	
 }
@@ -92,40 +103,23 @@ function drawMap(){
 		mapX=0;
 		mapY+=64;
 	}
-	
-	
-	
-	
-	
-	
-	if(charPos==0){
+
+    if(charDirection==SOUTH_DIRECTION){
+		context.drawImage(player, 0, 0, 64, 64, charX, charY, 64, 64);
+	}		
+	if(charDirection==WEST_DIRECTION){
 		context.drawImage(player, 0, 64, 64, 64, charX, charY, 64, 64);
 	}
-	if(charPos==1){
-		context.drawImage(player, 0, 192, 64, 64, charX, charY, 64, 64);
-	}
-	if(charPos==2){
+	if(charDirection==EAST_DIRECTION){
 		context.drawImage(player, 0, 128, 64, 64, charX, charY, 64, 64);
 	}
-	if(charPos==3){
-		context.drawImage(player, 0, 0, 64, 64, charX, charY, 64, 64);
+	if(charDirection==NORTH_DIRECTION){
+		context.drawImage(player, 0, 192, 64, 64, charX, charY, 64, 64);
 	}
-	
-	
+
 }
 
 setInterval(function fps(){
-
-	
-	
-	
-	
 	context.clearRect(0, 0, canvas.width, canvas.height);
-	
 	drawMap();
-
-	
-	
-	
-	
 }, 50);
