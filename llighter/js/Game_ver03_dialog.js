84b539588c9cd7ae9b9abd0ee6f44e81f134625b
sin <sinew8706@gmail.com>
2017-04-18 11:07:21 +0900

    npc블럭 수정 // 캐릭이미지 펑션정리 // 깨진주석삭제

--- llighter/js/Game_ver03_dialog.js
+++ llighter/js/Game_ver03_dialog.js
@@ -10,12 +10,12 @@ var map00=[
 	[0,0,0,0,0,1,1,1,0,0,0,0,0,0,101,0,0,0,0,0],
 	[0,0,0,0,0,0,0,1,1,0,101,101,101,101,101,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,10,0,0,1,1,0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,11,0,0,1,1,1,1,1,1,1,1,1,1,1,99],
-	[0,0,0,0,0,12,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,101,0,13,0,0,0,1,0,0,0,0,0,0,101,0,0,0,0],
-	[0,0,0,0,0,14,0,0,1,0,0,0,0,0,0,101,0,0,0,0],
-	[1,1,1,1,1,15,0,0,1,0,0,0,0,0,0,101,0,0,0,0],
+	[0,0,0,0,0,110,111,112,113,114,115,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1,1,99],
+	[0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,101,0,1,0,0,0,1,0,0,0,0,0,0,101,0,0,0,0],
+	[0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,101,0,0,0,0],
+	[1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,101,0,0,0,0],
 	[0,0,0,0,1,1,0,0,1,0,0,0,0,0,101,101,0,0,0,0],
 	[0,0,0,0,0,1,1,1,1,0,0,0,0,0,101,0,0,0,0,0],
 	[0,0,101,0,0,0,0,1,1,101,101,101,101,101,101,0,0,0,0,0],
@@ -50,7 +50,7 @@ var map01=[
 
 
 
-// �쁽�옱留� �꽑�뼵
+// 현재맵 선언
 var nowMap;
 nowMap = map00;
 
@@ -90,10 +90,6 @@ var player = new Image();
 
 // character motion
 
-const MOTION00 = 0;
-const MOTION01 = 1;
-const MOTION02 = 2;
-const MOTION03 = 3;
 var motionIdx = MOTION00;
 
 
@@ -177,49 +173,51 @@ function key(){
 			}
 	        charDirection = SOUTH_DIRECTION;
 		}
-		// npc를 바라보고 앞으로 간 후 엔터 키 누르면 dialog창이 뜸. 아직 npc가 없기에 맵에 npc가 있다고 가정하고    				       p (플레이어)
-		// 강사님은 10, 상점 11, 미션1npc 12, 미션2npc 13, 미션3npc 14, 최종보스 15로 맵에다 임의로 설정. 방향또한 그냥 남쪽을 바라보는 형태로 설정 --> 	npc  
+		// npc를 바라보고 앞으로 간 후 엔터 키 누르면 dialog창이 뜸. 아직 npc가 없기에 맵에 npc가 있다고 가정하고 p (플레이어)
+		// 강사님은 10, 상점 11, 미션1npc 12, 미션2npc 13, 미션3npc 14, 최종보스 15로 맵에다 임의로 설정.
+		// npc를 아래에서 바라보게 북쪽방향으로 수정
 		if(event.keyCode==13){
 			// 강사님
-			if(nowMap[charY/UNIT][charX/UNIT]==10 && charDirection == SOUTH_DIRECTION){
+			if(nowMap[ charY/UNIT -1  ][ charX/UNIT ]==110 && charDirection == NORTH_DIRECTION){
 				chat.style="block";
 				createDiag( individual[0] );
 				keyValue = TEXTKEY;
 			}
 			// 상점
-			if(nowMap[charY/UNIT][charX/UNIT]==11 && charDirection == SOUTH_DIRECTION){
+			if(nowMap[ charY/UNIT -1 ][ charX/UNIT ]==111 && charDirection == NORTH_DIRECTION){
 				chat.style="block";
 				createDiag( individual[1] );
 				keyValue = TEXTKEY;
 			}
 			// 미션1
-			if(nowMap[charY/UNIT][charX/UNIT]==12 && charDirection == SOUTH_DIRECTION){
+			if(nowMap[ charY/UNIT -1 ][ charX/UNIT ]==112 && charDirection == NORTH_DIRECTION){
 				chat.style="block";
 				createDiag( individual[2] );
 				keyValue = TEXTKEY;
 			}
 			// 미션2
-			if(nowMap[charY/UNIT][charX/UNIT]==13 && charDirection == SOUTH_DIRECTION){
+			if(nowMap[ charY/UNIT -1 ][ charX/UNIT ]==113 && charDirection == NORTH_DIRECTION){
 				chat.style="block";
 				createDiag( individual[3] );
 				keyValue = TEXTKEY;
 			}
 			// 미션3
-			if(nowMap[charY/UNIT][charX/UNIT]==14 && charDirection == SOUTH_DIRECTION){
+			if(nowMap[ charY/UNIT -1 ][ charX/UNIT ]==114 && charDirection == NORTH_DIRECTION){
 				chat.style="block";
 				createDiag( individual[4] );
 				keyValue = TEXTKEY;
 			}
 			// 최종보스
-			if(nowMap[charY/UNIT][charX/UNIT]==15 && charDirection == SOUTH_DIRECTION){
+			if(nowMap[ charY/UNIT -1 ][ charX/UNIT ]==115 && charDirection == NORTH_DIRECTION){
 				chat.style="block";
 				createDiag( individual[5] );
 				keyValue = TEXTKEY;
 			}
+			
 		}
 
 	
-		// map01濡� �씠�룞
+		// map01에서 맵이동
 		if( nowMap[(charY / UNIT)][(charX / UNIT)]==99 ){
 			nowMap=map01;
 			charX= (0*UNIT);
@@ -229,7 +227,6 @@ function key(){
 	    console.log(`[Absolute coordinate] (X, Y) = (${charX / UNIT}, ${charY / UNIT})`);
 	}else if(keyValue==TEXTKEY){
 		if(event.keyCode == 13){
-			// 정효야 여기서 텍스트창 지우면 된다!
 			$('#dialog').html("");
 			chat.style.display="none";
 			//  이렇게 하면 창은 지워지긴하는데 좀 더 손 봐야함
@@ -243,49 +240,43 @@ function key(){
 
 
 
+} // --------------- key끝 --------------
 
-}
 
 function drawMap(){
 	mapX=0;
 	mapY=0;
 
-    // 10x10嶺뚮씭�뒭野껓옙 �솻洹ｋ샍�굢�띿�ゅ뜝占� 嶺뚮씭踰딉옙�뭿爾욃뜝占� �뜝�럥�냱�뜝�럥�닣,�뜝�럡�맟�뜝�럩�젧
+
     if( charX/UNIT < 5) {
-        // 0,0 占쎈쐻占쎈윞�뙼占썲뜝�럥�맶�뜝�럥吏쀥뜝�럩援꿨뜝�럥鍮앾옙�쐻�뜝占� �뜝�럩�맳�뛾�룆梨룡뤃�뼹�쐻占쎈윥占쎄틯�뜝�럥�맶�뜝�럥�쑅占쎌젂�뜝占�.
         x_start = 0;
         x_end = x_start + 10;
         x_char = charX;
     } else if( charX/UNIT >= 5 && charX/UNIT < 16) {
-        // X占쎈쐻占쎈윥占쎄괴�뜝�럥�맶占쎈쐻�뜝占� �뜝�럥��耀붾굝梨멨뜝�룞�맀占쎌맶�뜝�럥吏쀥뜝�럩援뀐옙�쐻占쎈윥占쎄괵�뜝�럩�뮋�뜝�럥�솕�뜝�럩援� �뜝�럥�맶�뜝�럥�쑋�뜝�럥�듃�솾�꺂�뒩占쎌굨�뜝�럩�몝�뜝�럥�맶占쎈쐻�뜝占� 4�뜝�럥�맶�뜝�럥�쑅�뜝�룞�삕 �뜝�럥�맶�뜝�럥�쑋雅��굝�뺧옙�굲嶺뚮ㅏ�솇壤쏆�⑥쪎�넫�맦�럢�뜝�럥�맶�뜝�럥吏쀥뜝�럩援� 5�뜝�럥�맶�뜝�럥�쑅�뜝�룞�삕 �뜝�럩�맳�뛾�룆梨룡뤃�뼹�쐻占쎈윥占쎄틯�뜝�럥�맶�뜝�럥�쑅占쎌젂�뜝占�.
         x_start = (charX-(charX%UNIT))/UNIT - 5
         x_end = (charX-(charX%UNIT))/UNIT + 5
         x_char = (charX%UNIT) + 5 * UNIT;
     } else {    // X >= 16
-        // 10 占쎈쐻占쎈윞�뙼占썲뜝�럥�맶�뜝�럥吏쀥뜝�럩援꿨뜝�럥鍮앾옙�쐻�뜝占� �뜝�럩�맳�뛾�룆梨룡뤃�뼹�쐻占쎈윥占쎄틯�뜝�럥�맶�뜝�럥�쑅占쎌젂�뜝占�.
         x_start = 10;
         x_end = 20;
         x_char = charX - 10 * UNIT;
     }
 
     if(charY/UNIT < 5) {
-        // 0,0 占쎈쐻占쎈윞�뙼占썲뜝�럥�맶�뜝�럥吏쀥뜝�럩援꿨뜝�럥鍮앾옙�쐻�뜝占� �뜝�럩�맳�뛾�룆梨룡뤃�뼹�쐻占쎈윥占쎄틯�뜝�럥�맶�뜝�럥�쑅占쎌젂�뜝占�.
-        y_start = 0;
+    	y_start = 0;
         y_end = y_start + 10;
         y_char = charY;
     } else if( charY/UNIT >= 5 && charY/UNIT < 16) {
-        // Y占쎈쐻占쎈윥占쎄괴�뜝�럥�맶占쎈쐻�뜝占� �뜝�럥��耀붾굝梨멨뜝�룞�맀占쎌맶�뜝�럥吏쀥뜝�럩援뀐옙�쐻占쎈윥占쎄괵�뜝�럩�뮋�뜝�럥�솕�뜝�럩援� �뜝�럥�맶�뜝�럥�쑋嶺뚮씧�맻占쎌맶�뜝�럥�쑅�뜏類㏃삕 4�뜝�럥�맶�뜝�럥�쑅�뜝�룞�삕 �뜝�럥�맶�뜝�럥�쑅�뜝�럥�뼀�뜝�럥�맶�뜝�럥�쑋�뜝�럡�뜲�뜝�럥�맶�뜝�럥�쑅�뜏類㏃삕 5�뜝�럥�맶�뜝�럥�쑅�뜝�룞�삕 �뜝�럩�맳�뛾�룆梨룡뤃�뼹�쐻占쎈윥占쎄틯�뜝�럥�맶�뜝�럥�쑅占쎌젂�뜝占�.
         y_start = (charY-(charY%UNIT))/UNIT - 5
         y_end = (charY-(charY%UNIT))/UNIT + 5
         y_char = (charY%UNIT) + (5 * UNIT);
     } else {    // Y >= 16
-        // 10 占쎈쐻占쎈윞�뙼占썲뜝�럥�맶�뜝�럥吏쀥뜝�럩援꿨뜝�럥鍮앾옙�쐻�뜝占� �뜝�럩�맳�뛾�룆梨룡뤃�뼹�쐻占쎈윥占쎄틯�뜝�럥�맶�뜝�럥�쑅占쎌젂�뜝占�.
         y_start = 10;
         y_end = 20;
         y_char = charY - 10 * UNIT;
     }
 	
-    // �댖怨뚰�쀦뤃酉귥삕�굢占� �뜝�럥�렊�뜝�럩逾� 嶺뚮씭踰뤷뜝�룞�삕占쎈뎄 �윜諛몄굡占쎈뉴�뼨�먯삕
+
 	for(var i = y_start; i < y_end ; i++){
 		for(var j=x_start, mapX = 0; j < x_end ; j++){
             switch(nowMap[i][j]) {
@@ -317,43 +308,18 @@ function drawMap(){
 
 }
 
-
+// 이미지파일이 규칙적이라 정리
 function drawChar(){
 	switch(charDirection) {
     case SOUTH_DIRECTION:
-    	switch(motionIdx){
-    		case MOTION00: context.drawImage(player, IMG_U*0, IMG_U*0, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    		case MOTION01: context.drawImage(player, IMG_U*1, IMG_U*0, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    		case MOTION02: context.drawImage(player, IMG_U*2, IMG_U*0, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    		case MOTION03: context.drawImage(player, IMG_U*3, IMG_U*0, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    	}
-        break;
+    	context.drawImage(player, IMG_U*motionIdx, IMG_U*0, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
     case WEST_DIRECTION:
-    	switch(motionIdx){
-    		case MOTION00: context.drawImage(player, IMG_U*0, IMG_U*1, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    		case MOTION01: context.drawImage(player, IMG_U*1, IMG_U*1, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    		case MOTION02: context.drawImage(player, IMG_U*2, IMG_U*1, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    		case MOTION03: context.drawImage(player, IMG_U*3, IMG_U*1, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    	}
-        break;
+    	context.drawImage(player, IMG_U*motionIdx, IMG_U*1, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
     case EAST_DIRECTION:
-    	switch(motionIdx){
-    		case MOTION00: context.drawImage(player, IMG_U*0, IMG_U*2, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    		case MOTION01: context.drawImage(player, IMG_U*1, IMG_U*2, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    		case MOTION02: context.drawImage(player, IMG_U*2, IMG_U*2, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    		case MOTION03: context.drawImage(player, IMG_U*3, IMG_U*2, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    	}
-        break;
+    	context.drawImage(player, IMG_U*motionIdx, IMG_U*2, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
     case NORTH_DIRECTION:
-    	switch(motionIdx){
-    		case MOTION00: context.drawImage(player, IMG_U*0, IMG_U*3, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    		case MOTION01: context.drawImage(player, IMG_U*1, IMG_U*3, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    		case MOTION02: context.drawImage(player, IMG_U*2, IMG_U*3, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    		case MOTION03: context.drawImage(player, IMG_U*3, IMG_U*3, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
-    	}
-        break;
+    	context.drawImage(player, IMG_U*motionIdx, IMG_U*3, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT); break;
 	}
-
 }
 
 	setInterval(function fps(){
