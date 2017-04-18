0dc9385d995d28065ab922f21ed68726df9f6ad8
llighter <hadbsgk@gmail.com>
2017-04-18 11:43:40 +0900

    [Bug fix]캐릭터 움직임을 작게 하면서 발생하는 문제 해결 - 관용이형 코드 참고

--- llighter/js/Game.js
+++ llighter/js/Game.js
@@ -2,32 +2,56 @@
 var canvas = document.getElementById("startVillage");
 var context = canvas.getContext("2d");
 
-var map=[
-	[0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0],
-	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0],
-	[1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0],
-	[0,0,0,0,1,1,0,0,0,0,0,0,0,0,2,2,0,0,0,0],
-	[0,0,0,0,0,1,1,1,0,0,0,0,0,0,2,0,0,0,0,0],
-	[0,0,0,0,0,0,0,1,1,2,2,2,2,2,2,0,0,0,0,0],
+var map00=[
+	[0,0,101,0,0,0,0,0,0,0,0,0,0,0,0,101,0,0,0,0],
+	[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,101,0,0,0,0],
+	[1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,1,1,0,0,0,0,0,0,0,0,101,101,0,0,0,0],
+	[0,0,0,0,0,1,1,1,0,0,0,0,0,0,101,0,0,0,0,0],
+	[0,0,0,0,0,0,0,1,1,0,101,101,101,101,101,0,0,0,0,0],
 	[0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0],
-	[0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1],
-	[0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,2,0,0,0,0,0,1,0,0,0,0,0,0,2,0,0,0,0],
-	[0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,2,0,0,0,0],
-	[1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,2,0,0,0,0],
-	[0,0,0,0,1,1,0,0,1,0,0,0,0,0,2,2,0,0,0,0],
-	[0,0,0,0,0,1,1,1,1,0,0,0,0,0,2,0,0,0,0,0],
-	[0,0,2,0,0,0,0,1,1,2,2,2,2,2,2,0,0,0,0,0],
-	[0,0,2,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
-	[0,0,2,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0],
-	[0,0,2,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1],
-	[0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	[0,0,0,0,0,101,0,0,1,1,0,0,0,0,0,0,0,0,0,0],
+	[0,0,0,0,0,101,0,0,1,1,1,1,1,1,1,1,1,1,1,99],
+	[0,0,0,0,0,101,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,101,0,101,0,0,0,1,0,0,0,0,0,0,101,0,0,0,0],
+	[0,0,0,0,0,101,0,0,1,0,0,0,0,0,0,101,0,0,0,0],
+	[1,1,1,1,1,101,0,0,1,0,0,0,0,0,0,101,0,0,0,0],
+	[0,0,0,0,1,1,0,0,1,0,0,0,0,0,101,101,0,0,0,0],
+	[0,0,0,0,0,1,1,1,1,0,0,0,0,0,101,0,0,0,0,0],
+	[0,0,101,0,0,0,0,1,1,101,101,101,101,101,101,0,0,0,0,0],
+	[0,0,101,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,101,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0],
+	[0,0,101,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1],
+	[0,0,101,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+];
+
+var map01=[
+	[3,3,101,0,3,3,3,0,0,0,0,0,0,0,0,101,0,0,0,0],
+	[0,0,102,3,3,0,0,0,0,0,0,0,0,0,0,101,0,0,0,0],
+	[4,4,102,1,1,0,0,0,0,0,0,0,0,0,0,101,0,0,0,0],
+	[0,0,4,3,1,1,0,0,0,0,0,0,0,0,101,101,0,0,0,0],
+	[101,101,4,3,0,1,1,1,0,0,0,0,0,0,101,0,0,0,0,0],
+	[102,102,4,3,0,0,0,1,1,101,102,102,102,102,102,0,0,0,0,0],
+	[0,0,4,3,0,0,0,0,1,0,0,0,0,0,3,3,3,0,0,0],
+	[0,0,4,3,0,0,0,0,1,1,4,4,4,4,4,4,4,0,0,0],
+	[0,0,4,3,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1],
+	[0,0,4,3,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,4,0,0,0,0,0,1,0,0,0,0,0,0,101,0,0,0,0],
+	[0,0,4,0,0,0,0,0,1,0,0,0,0,0,0,101,0,0,0,0],
+	[1,1,4,4,4,3,3,3,1,0,0,0,0,0,0,101,0,0,0,0],
+	[0,0,4,0,1,1,0,0,1,0,0,0,0,0,101,101,0,0,0,0],
+	[0,0,4,0,0,1,1,1,1,0,0,0,0,0,101,0,0,0,0,0],
+	[0,0,4,0,0,0,0,1,1,5,5,5,101,101,101,0,0,0,0,0],
+	[0,0,4,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0],
+	[0,0,102,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0],
+	[0,0,102,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1],
+	[0,0,102,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ];
 
 const UNIT = 64;
 const IMG_U = 96;
 const MAPIMG_U = 64;
+const MOVE_U = 16;
 
 const ARROW_LEFT = 37;
 const ARROW_UP = 38;
@@ -41,21 +65,29 @@ const NORTH_DIRECTION = 3;
 
 const MAP_GRASS = 0;
 const MAP_ROAD01 = 1;
-const MAP_STONE = 2;
+const MAP_STONE = 101;
+const MAP_SAND = 3;
+const MAP_SANDROAD01 = 4;
+const MAP_SANDSTONE = 102;
 
+var sand = new Image();
 var grass = new Image();
+var stone = new Image();
 var road01 = new Image();
-var rock = new Image();
 var player = new Image();
+var sandStone = new Image();
+var sandRoad01 = new Image();
 
 // Charactor's direction
 var motionIdx = 0;
 
-
-grass.src = './img/tileImage.png';
-road01.src = './img/tileImage.png';
-rock.src = './img/tileImage.png';
 player.src = './img/eagle.png';
+sand.src = './img/tileImage.png';
+grass.src = './img/tileImage.png';
+stone.src = './img/tileImage.png';
+road01.src = './img/tileImage.png';
+sandStone.src = './img/tileImage.png';
+sandRoad01.src = './img/tileImage.png';
 
 // Map coordinate
 var mapX = 0;
@@ -66,6 +98,7 @@ var charX = 0;
 var charY = 0;
 
 // Current map index
+var nowMap = map00;
 var x_start = 0;
 var x_end = 0;
 var x_char = 0;
@@ -123,25 +156,33 @@ function collisionDetection() {
 	let detector = false;
 
 	if(upPressed == true) {
-		if((charY <= 0) || map[(charY / UNIT) - 1][(charX / UNIT)] == MAP_STONE) {
+		if( (charY < MOVE_U )
+			|| nowMap[ Math.ceil( (charY - UNIT) / UNIT) ][((charX-(charX%UNIT)) / UNIT)] > 100
+			|| nowMap[ Math.ceil( (charY - UNIT) / UNIT) ][ Math.ceil(charX / UNIT) ] > 100 ){
 			detector = true;
 		}
 	}
 
 	if(downPressed == true) {
-		if((charY >= 19 * UNIT) || map[(charY / UNIT) + 1][(charX / UNIT)] == MAP_STONE) {
+		if( (charY > (19 * UNIT - MOVE_U) )
+			|| nowMap[ ( (charY-(charY%UNIT)+UNIT) / UNIT) ][ ( (charX - (charX%UNIT) ) / UNIT)] > 100 
+			|| nowMap[ ( (charY-(charY%UNIT)+UNIT) / UNIT) ][ Math.ceil( charX / UNIT ) ] > 100 ){
 			detector = true;
 		}
 	}
 
 	if(leftPressed == true) {
-		if((charX <= 0) || map[(charY / UNIT)][(charX / UNIT) - 1] == MAP_STONE) {
+		if( (charX <= 0)
+			|| nowMap[((charY-(charY%UNIT)) / UNIT)][ Math.ceil((charX-UNIT) / UNIT) ] > 100
+			|| nowMap[ Math.ceil(charY / UNIT) ][ Math.ceil((charX-UNIT) / UNIT)] > 100 ){
 			detector = true;
 		}
 	}
 
 	if(rightPressed == true) {
-		if((charX >= 19 * UNIT) || map[(charY / UNIT)][(charX / UNIT) + 1] == MAP_STONE) {
+		if( (charX > (19 * UNIT - MOVE_U) )
+			|| nowMap[((charY-(charY%UNIT)) / UNIT)][((charX-(charX%UNIT) + UNIT) / UNIT)] > 100
+			|| nowMap[ Math.ceil(charY / UNIT) ][((charX-(charX%UNIT) + UNIT) / UNIT)] > 100 ){
 			detector = true;
 		}
 	}
@@ -152,22 +193,22 @@ function collisionDetection() {
 
 function move() {
 	if(upPressed == true) {
-		charY -= collisionDetection() ? 0 : UNIT;
+		charY -= collisionDetection() ? 0 : MOVE_U;
 		charDirection = NORTH_DIRECTION;
 	}
 
 	if(downPressed == true) {
-		charY += collisionDetection() ? 0 : UNIT;
+		charY += collisionDetection() ? 0 : MOVE_U;
 		charDirection = SOUTH_DIRECTION;
 	}
 
 	if(leftPressed == true) {
-		charX -= collisionDetection() ? 0 : UNIT;
+		charX -= collisionDetection() ? 0 : MOVE_U;
 		charDirection = WEST_DIRECTION;
 	}
 
 	if(rightPressed == true) {
-		charX += collisionDetection() ? 0 : UNIT;
+		charX += collisionDetection() ? 0 : MOVE_U;
 		charDirection = EAST_DIRECTION;
 	}
 
@@ -175,28 +216,28 @@ function move() {
 }
 
 function setMap() {
-	if(charX/UNIT <= 4) {
+	if(charX/UNIT < 5) {
         x_start = 0;
         x_end = x_start + 10;
         x_char = charX;
     } else if( charX/UNIT >= 5 && charX/UNIT < 16) {
-        x_start = charX/UNIT - 5
-        x_end = charX/UNIT + 5
-        x_char = 5 * UNIT;
+        x_start = (charX-(charX%UNIT))/UNIT - 5
+        x_end = (charX-(charX%UNIT))/UNIT + 5
+        x_char = (charX%UNIT) + 5 * UNIT;
     } else {    // X >= 16
         x_start = 10;
         x_end = 20;
         x_char = charX - 10 * UNIT;
     }
 
-    if(charY/UNIT <= 4) {
+    if(charY/UNIT < 5) {
         y_start = 0;
         y_end = y_start + 10;
         y_char = charY;
     } else if( charY/UNIT >= 5 && charY/UNIT < 16) {
-        y_start = charY/UNIT - 5
-        y_end = charY/UNIT + 5
-        y_char = 5 * UNIT;
+        y_start = (charY-(charY%UNIT))/UNIT - 5
+        y_end = (charY-(charY%UNIT))/UNIT + 5
+        y_char = (charY%UNIT) + (5 * UNIT);
     } else {    // Y >= 16
         y_start = 10;
         y_end = 20;
@@ -213,7 +254,7 @@ function draw(){
 	
 	for(var i = y_start; i < y_end ; i++){
 		for(var j=x_start, mapX = 0; j < x_end ; j++){
-            switch(map[i][j]) {
+            switch(nowMap[i][j]) {
                 case MAP_GRASS:
                     context.drawImage(grass, 0, 32, MAPIMG_U, MAPIMG_U, mapX, mapY, UNIT, UNIT);
                     break;
@@ -221,13 +262,23 @@ function draw(){
                     context.drawImage(road01, 64, 32, MAPIMG_U, MAPIMG_U, mapX, mapY, UNIT, UNIT);
                     break;
                 case MAP_STONE:
-                    context.drawImage(rock, 192, 32, MAPIMG_U, MAPIMG_U, mapX, mapY, UNIT, UNIT);
+                    context.drawImage(stone, 192, 32, MAPIMG_U, MAPIMG_U, mapX, mapY, UNIT, UNIT);
+                    break;
+                case MAP_SAND:
+                    context.drawImage(sand, 0, 128, MAPIMG_U, MAPIMG_U, mapX, mapY, UNIT, UNIT);
+                    break;
+                case MAP_SANDROAD01:
+                    context.drawImage(sandRoad01, 64, 128, MAPIMG_U, MAPIMG_U, mapX, mapY, UNIT, UNIT);
+                    break;
+                case MAP_SANDSTONE:
+                    context.drawImage(sandStone, 128, 128, MAPIMG_U, MAPIMG_U, mapX, mapY, UNIT, UNIT);
                     break;
             }
             mapX += UNIT;
 		}
 		mapY += UNIT;
 	}
+
 	context.drawImage(player, IMG_U*motionIdx, IMG_U*charDirection, IMG_U, IMG_U, x_char, y_char, UNIT, UNIT);
 
 	// requestAnimationFrame(draw);
