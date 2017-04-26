9e76c98ae1eb9a6445a2624016f2b9593f2d13d6
llighter <hadbsgk@gmail.com>
2017-04-26 11:36:53 +0900

    [map]파일 기능 별 분리 및 정리

--- AllForOne/js/Game.js
+++ AllForOne/js/Game.js
@@ -1,194 +1,14 @@
-// INIT
+
 var canvas = document.getElementById("village");
 var context = canvas.getContext("2d");
 
-var map_init = [
-    [   0,   0,   0,   0,   0,   0,   0,   0,   0, 210],
-    [ 202, 203, 204,   0,   0,   0, 501,   0, 205,2101],
-    [2021,2031,2041,   0,   0,   0,   0, 206, 206,2102],
-    [   0,   0,   0,   0,   0,   0,   0,   0,   0, 211],
-    [ 207,   0,  207,  0, 207,   0, 207,   0, 207,2111],
-    [2071, 209, 2071,209,2071,   0,2071, 209,2071, 211],
-    [ 208,   0,  208,  0, 208,   0, 208,   0, 208,2111],
-    [ 207,   0,  207,  0, 207,   0, 207,   0, 207, 211],
-    [2071, 209, 2071,209,2071,   0,2071, 209,2071,2111],
-    [208,    0, 208,   0, 208,  90, 208,   0, 208,   0]
-];
-
-var map_boss = [
-    [ 301, 301, 301, 301, 301, 301, 301, 301, 301, 301],
-    [ 301, 301,	  0,   0,   0,   0,   0,   0, 301, 301],
-    [ 301, 301,   0,   0, 303,3021,   0,   0, 301, 301],
-    [ 301, 301,   0,   0,   0,   0,   0,   0, 301, 301],
-    [ 301, 301,   0,   0,   0,   0,   0,   0, 301, 301],
-    [ 301, 301,   0,   0,   0,   0,   0,   0, 301, 301],
-    [ 301, 301, 301, 301,   0,   0, 301, 301, 301, 301],
-    [ 301, 301, 301, 301,   0,   0, 301, 301, 301, 301],
-    [  94,   0,   0,   0,   0,   0, 301, 301, 301, 301],
-    [ 301, 301, 301, 301, 301, 301, 301, 301, 301, 301]
-];
-
-var map00=[
-	   [154,154,154,154,154,154,154,154,154,154,154,154,154,154,154,154,154,154,154,154],
-	   [154,  0,  0,210,100,100,100,100,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,154],
-	   [154,  0,  0,100,100,100,100,100,  0,  0,  0, 50, 50, 50, 50, 50, 50,  0,  0,154],
-	   [154,  0,  0,100,100,100,100,100,  0,  0,  0, 50, 50, 50, 50, 50, 50,  0,  0,154],
-	   [154,  0,  0,100,100,100,100,100,  0,  0,  0, 50, 50, 50, 50, 50, 50,  0,  0,154],
-	   [154,  0,  0,100,100,91,100,100,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,154],
-	   [154,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  1,  0,  0,200,100,  0,  0,154],
-	   [154,  0,  0,  0,  0,  1,  0,  0,  0,  0,  0,  0,  1,  0,  0,100,100,155,  0,154],
-	   [154,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  502,  0,  0,154],
-	   [154,  0,  0,  0,  0,  1,  0,  0,  0,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,154],
-	   [154,  0,  0, 50, 50, 50, 50, 50,  0,  1,  1,  0,  0,230,100,  0,  0,  0,  0,154],
-	   [154,  0,  0, 50, 50, 50, 50, 50,  0,  1,  1,  0,  0,100,100,  0,  0,  0,  0,154],
-	   [154,  0,  0, 50, 50, 50, 50, 50,  0,  1,  1,  1,  1,  1,  503,  1,  1,  1,  1, 99],
-	   [154,  0,220,100,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, 99],
-	   [154,  0,100,100,  0,  0,  0,  0,  0,  1,  1,  0,  0,  0,  0,  1,  0,  0,  0,154],
-	   [154,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  0,  0,  1,  0,  0,  0,154],
-	   [154,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 50, 50, 50, 50, 50, 50,154],
-	   [154,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 50, 50, 50, 50, 50, 50,154],
-	   [154,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 50, 50, 50, 50, 50, 50,154],
-	   [154,154,154,154,154,154,154,154,154,154,154,154,154,154,154,154,154,154,154,154]
-
-	];
-
-
-var map01=[
-	   [  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,110, 97, 97,110,  3,  3,  3,  3],
-	   [  3,  3,  3,  3,  3,  3,110,110,110,110,110,110,110,  5,  5,110,110,110,110,110],
-	   [ 52, 52, 52,  3,  3,  3,110, 51, 51,  3,  3,  3,  3,  5,  5,  3,  3,  3,  3,110],
-	   [ 52, 52, 52,  3,  3,  3,110, 51, 51,  3,  3,200,100,  5,  5,  3,  3,  3,  3,110],
-	   [ 52, 52, 52,  3,  3,  3,110, 51, 51,  3,  3,100,100,  5,  5,  3,  3,  3,  3,110],
-	   [ 52, 52, 52,  3,  3,  3,110,  3,  3,  3,  3,  3,  504,  5,  5,  3,  3,220,  3,110],
-	   [ 52, 52, 52,  3,  3,  3,110,  3,230,100,  3,  3,  3,  5,  5,  3,  3,  3,  3,110],
-	   [  3,  3,  3,  3,  3,  3,110,  3,100,100,  505,  3,  3,  5,  5,  3,  3,  3,  3,110],
-	   [  3,  3,  3,  3,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  3,  3,  3,  3,110],
-	   [  3,  3,  3,  3,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  5,  3,240,  3,  3,110],
-	   [  3,  3,  3,  3,  5,  3,110,  3,  3,  3,  3,  3,  3,  3,  3,  3,100,  3,  3,110],
-	   [  3,  3,  3,  3,  5,  3,110,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,110],
-	   [  3,  3,  3,  3,  5,  3,110,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,  3,110],
-	   [  3,  3,  3,  3,  5,  3,110,  3, 51, 51, 51, 51, 51,  3,  3,  3, 51, 51, 51,110],
-	   [154,  3,  3,  3,  5,  3,110,  3, 51, 51, 51, 51, 51,  3,  3,  3, 51, 51, 51,110],
-	   [154,154,  3,  3,  5,  3,110,  3, 51, 51, 51, 51, 51,  3,  3,  3, 51, 51, 51,110],
-	   [ 98,  4,  4,  4,  5,  3,110,110,110,110,110,110,110,110,110,  3,110,110,110,110],
-	   [ 98,  4,  4,  4,  4,  3,  3,  3,  3, 52, 52, 52, 52, 52, 52,  3,  3,  3,  3,  3],
-	   [154,154,  3,  3,  3,  3, 52, 52,  3, 52, 52, 52, 52, 52, 52,  3,  3, 52, 52, 52],
-	   [154,154,154,  3,  3,  3, 52, 52,  3,  3,  3,  3,  3,  3,  3,  3,  3, 52, 52, 52]
-
-	];
-	
-	
-var map02=[
-	   [152,152,152,152,152,152,152,152,152,152,152,152,152,152,152,152,152,152,152,152],
-	   [152,230,100,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,156,155,152,152,152,152,152],
-	   [152,100,100,156,  8,  8,  8,  8,  8,  8,  8,  7,  7,  7,  7,  7,  7,  7,  7,152],
-	   [  7,  507,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  7,  7,152],
-	   [  7,  7,  8,  8,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  8,  7,  7,152],
-	   [  7,  7,  8,  8,  7,  7,  7,  7,152,152,  7,  7,  7,  7,  7,  7,  8,  7,  7,152],
-	   [  7,  7,  8,  8, 53, 53, 53, 53,152,152,152,152,152,  7,  7,  7,  8,  7, 54, 54],
-	   [  7,  7,  8,  8, 53, 53, 53,152,152,152,152,152,240,100,100,  7,  8,  7, 54, 54],
-	   [  7,  7,  8,  8, 53, 53, 53,152,152,  7,  7,155,100,100,100,  7,  8,  7, 54, 54],
-	   [  7,  7,  8,  8,152,152,152,152,152,  7,  7,  7,  7,  7,  7,  7,  8,  7, 54, 54],
-	   [  7,  7,  8,  8,152,152,152,152,152, 53, 53,  7,  8,  8,  8,  8,  8,152,152,152],
-	   [  7,  7,  8,  8,152,152,152,152,152, 53, 53,  7,  8,152,152,152,152,152,152,152],
-	   [  7,  7,  8,  8,  8,  7,200,100,152, 53, 53,  7,  8,152,152,152,152,152,152,152],
-	   [  7,  7,  7,  8,  8,  7,100,100,152,  7,  7,  8,  8,152,152,152,152,152,152,152],
-	   [ 53, 53,  7,  8,  8,  8,  8,  506,152,  7,  7,  8,  7, 54, 54, 54, 54,  7,152,152],
-	   [ 53, 53,  7,  8,  8,152,152,152,152,  7,  7,  8,  7, 54, 54, 54, 54,  7,152,152],
-	   [ 53, 53,  7,  8,  8,152,152,  7,220,100,  7,  8,  7,  7,  7,  7,  7,  7,  7,152],
-	   [ 53, 53,  7,  8,  8,152,152,  7,100,100,  7,  8,  8,  8,  8,  8,  8,  8,  8, 95],
-	   [ 53, 53,  7,  8,  8,152,152,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  8, 95],
-	   [152,152,152, 96, 96,152,152,152,152,152,152,152,152,152,152,152,152,152,152,152]
-
-	];	
-
-
-const UNIT = 64;
-const IMG_U = 96;
-const MAPIMG_U = 64;
-const MOVE_U = 16;
-
-const ARROW_LEFT = 37;
-const ARROW_UP = 38;
-const ARROW_RIGHT = 39;
-const ARROW_DOWN = 40;
-const SPACE_BAR = 32;
-
-const SOUTH_DIRECTION = 0;
-const WEST_DIRECTION = 1;
-const EAST_DIRECTION = 2;
-const NORTH_DIRECTION = 3;
-
-const MAP_WIDTH = 640;
-const MAP_HEIGHT = 640;
-const MAP_GRASS = 0;
-const MAP_ROAD01 = 1;
-const MAP_STONE = 101;
-const MAP_SAND = 3;
-const MAP_SANDROAD01 = 4;
-const MAP_SANDSTONE = 102;
-const MAP_NPC_MON = 200;
-const MAP_NPC_MON2 = 201;
-
-var player = new Image();
-var monster = new Image();
-var monster2 = new Image();
-var currentVillage = new Image();
-var academy = new Image();
-var village00 = new Image();
-var village01 = new Image();
-var village02 = new Image();
-var bossmap = new Image();
-
-// Charactor's direction
-var motionIdx = 0;
-
-// Monster's direction
-var monsterIdx = 0;
-
-player.src = './img/map/eagle.png';
-monster.src = './img/map/mon00.png';
-monster2.src = './img/map/mon01.png';
-currentVillage.src = './img/map/map_academy_v2.png';
-academy.src = './img/map/map_academy.png';
-village00.src = './img/map/stage00_npc.png';
-village01.src = './img/map/stage01_npc.png';
-village02.src = './img/map/stage02_npc.png';
-bossmap.src = './img/map/bossmap.png';
-
-
-
-function Player(id, name, x, y, img, direction) {
-	this.id = id;
-    this.name = name;
-    this.x = x;
-    this.y = y;
-    this.img = img;
-	this.direction = direction;
-}
-
-function Monster(id, name, x, y, img) {
-	this.id = id;
-    this.name = name;
-    this.x = x;
-    this.y = y;
-    this.img = img;
-}
-
-function Map(id, img, width, height, mappingArray) {
-	this.id = id;
-	this.img = img;
-	this.width = width;
-	this.height = height;
-	this.mappingArray = mappingArray;
-}
 
 var myPlayer = new Player('player01', 'yunha', UNIT*4 ,UNIT*2, player, EAST_DIRECTION);
 
+
+// TODO : 아직 사용하지 않음
 var mapList = [];
 var monsterList = {};
-
-// Not Yet used...
 mapList.push(new Map('00', academy, 640, 640, map_init));
 mapList.push(new Map('01', village00, 1280, 1280, map00));
 mapList.push(new Map('02', village01, 1280, 1280, map01));
@@ -214,18 +34,11 @@ var option=document.getElementById("option");
 // 초기값 opening을 위해 2로 조정 opening멘트 끝나면 0으로 변경
 var currentMode = 2;
 
-var upPressed = false;
-var downPressed = false;
-var leftPressed = false;
-var rightPressed = false;
-
 
 var textOn=0;
 var onPokemonZone = false;
 var battleCountDown = 4;
 
-document.addEventListener("keyup", keyUpHandler, false);
-document.addEventListener("keydown", keyDownHandler, false);
 
 document.addEventListener('keyup', (event) => {
   if (event.keyCode === 32 && textOn == 0) {
@@ -276,7 +89,7 @@ document.addEventListener('keyup', (event) => {
 			break;
 
 	}
-  }else if(event.keyCode === 49) {
+  } else if(event.keyCode === KEYBOARD_1) {
 	  clearDiag();
 	  // currentMode값을 0으로 변경(opening에 사용)
 	  currentMode = 0;
@@ -288,17 +101,17 @@ document.addEventListener('keyup', (event) => {
 // *2키-민트 *3키-포켓볼 *4키-치료 *5키-방생 
   if(textOn==2){
 	  switch(event.keyCode){
-	  case 50: store("mint"); break;
-	  case 51: store("pokeBall"); break;
-	  case 52: store("heal"); break;
-	  case 53: store("makeMonFree"); break;
+	  case KEYBOARD_2: store("mint"); break;
+	  case KEYBOARD_3: store("pokeBall"); break;
+	  case KEYBOARD_4: store("heal"); break;
+	  case KEYBOARD_5: store("makeMonFree"); break;
 		  
 	  }
   }
 // 퀘스트. textOn=3이고 2번 눌렀을때 퀘스트대화창발생 
 // 퀘스트는 심각한 오류가 있음. 2번키누르면 퀘스트 수락인데 이게 중복으로 계속 발생
 // 예를 들어 스테이지 2퀘스트가 몬스터볼 보상으로 얻는건데 2번키계속누르면 무한으로 얻을수 있음  
-  if(textOn==3 && event.keyCode==50){
+  if(textOn==3 && event.keyCode == KEYBOARD_2 ){
 	  switch(npcDetection()){
 	  case 503: getQuest(1); $('#option').html("[1] ㅂㅂ"); break;
 	  case 505: getQuest(2); $('#option').html("[1] ㅂㅂ"); break;
@@ -311,44 +124,10 @@ document.addEventListener('keyup', (event) => {
 
 // A키 눌렀을 때
 document.addEventListener('keyup', (event) => {
-  if (event.keyCode === 65) {
+  if (event.keyCode === KEYBOARD_A) {
 	currentMode = 4;
   }
 }, false);
-
-function keyDownHandler(e) {
-	switch(e.keyCode) {
-		case ARROW_LEFT:
-			leftPressed = true;
-			break;
-		case ARROW_UP:
-			upPressed = true;
-			break;
-		case ARROW_RIGHT:
-			rightPressed = true;
-			break;
-		case ARROW_DOWN:
-			downPressed = true;
-			break;
-	}
-}
-
-function keyUpHandler(e) {
-    switch(e.keyCode) {
-		case ARROW_LEFT:
-			leftPressed = false;
-			break;
-		case ARROW_UP:
-			upPressed = false;
-			break;
-		case ARROW_RIGHT:
-			rightPressed = false;
-			break;
-		case ARROW_DOWN:
-			downPressed = false;
-			break;
-	}
-}
 
 // Check whether next move is blocked
 function collisionDetection() {
@@ -539,18 +318,6 @@ for(var idx = 0; idx < init_talk.length; idx++) {
 	console.log(temp[idx]);
 }
 
-// // npc 대화 정의. 임의로 박사님(강사님), 상점, 던전1 미션주는npc, 던전2, 던전3, 짱짱보스jquery몬
-// var talk = ['짱짱개발자가 되서 돌아와라!', '상점입니다.', 'h1몬 5마리 잡아오세요', 'div몬 10마리 잡아와라', '뒤지기시름 table몬5마리 잡아와라', '안녕? 난짱짱강한 최종보스 jquery몬이라고 한다!'];
-
-// // 대화 한단어로 분할한 것 배열 정의
-// var individual=[];
-
-// // for문으로 각 npc별 대화를 모조리 한단어씩 쪼개버림.
-// // 강사님은 index 0, 상점 index 1, 던전1미션 index2, 던전2미션 index3, 던전3미션 index4, 최종보스 index5
-// for(z=0; z<talk.length; z++){
-// 	individual[z] = talk[z].split(' ');
-// };
-
 // dialog창에 text 출력
 function createDiag ( dialog ) {
 	for(k = 0; k < dialog.length; k++) {
@@ -623,10 +390,6 @@ var update = setInterval(function fps(){
 setInterval(function motionFps(){
 	motionIdx=(motionIdx+1) % 4
 }, 150);
-
-setInterval(function motionFps(){
-	monsterIdx=(monsterIdx+1) % 4
-}, 3000);
 
 var countDown =  setInterval(function() {
 	if(onPokemonZone == true) {
---
+++ AllForOne/js/map/KeyControl.js
@@ -0,0 +1,41 @@
+var upPressed = false;
+var downPressed = false;
+var leftPressed = false;
+var rightPressed = false;
+
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("keydown", keyDownHandler, false);
+
+function keyDownHandler(e) {
+	switch(e.keyCode) {
+		case ARROW_LEFT:
+			leftPressed = true;
+			break;
+		case ARROW_UP:
+			upPressed = true;
+			break;
+		case ARROW_RIGHT:
+			rightPressed = true;
+			break;
+		case ARROW_DOWN:
+			downPressed = true;
+			break;
+	}
+}
+
+function keyUpHandler(e) {
+    switch(e.keyCode) {
+		case ARROW_LEFT:
+			leftPressed = false;
+			break;
+		case ARROW_UP:
+			upPressed = false;
+			break;
+		case ARROW_RIGHT:
+			rightPressed = false;
+			break;
+		case ARROW_DOWN:
+			downPressed = false;
+			break;
+	}
+}
