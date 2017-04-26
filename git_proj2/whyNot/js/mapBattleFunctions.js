b0fa67e249f13570107a69f22ad22279a9199f04
JohnBay <moneymashi@gmail.com>
2017-04-26 10:43:11 +0900

    ++apr25, 10:42 지우상태창 골드버그.

--- git_proj2/whyNot/js/mapBattleFunctions.js
+++ git_proj2/whyNot/js/mapBattleFunctions.js
@@ -65,7 +65,7 @@ pokemons.push(new MyPokemon((bookNumber++), 0, "HTML", 		2, 0, 50, 13, 1, 0 ,"no
 var jiwoo = {name: "한지우", age: 16 , mint:5, pokeBall:4, golds:30000};
 $(".whyStatusNamebox").html("이름: "+jiwoo.name + "<br/> 나이: "+jiwoo.age )
 .css({"font-size": "30pt", "font-weight": "bolder"});
-$(".whyStatusMoneybox").html("소유 골드: "+ jiwoo.golds +"골드 "+"<br/> 민트: "+ jiwoo.mint +"개 <br/>몬스터볼: "+ jiwoo.pokeBall+ "개")
+$(".whyStatusMoneybox").html("소유 골드: "+ jiwoo.golds +"골드 "+"<br/> 민트: "+ jiwoo.mint +"개 <br/>몬스터볼: "+ jiwoo.pokeBall+ "개");
 .css({"font-size": "20pt", "font-weight": "bolder"});
 //console.log("jiwoo.mint "+jiwoo.mint +"  jiwoo.pokeBall "+ jiwoo.pokeBall);
 
@@ -222,6 +222,7 @@ function store(wantedService){
 		} /////////
 		
 	}
+	$(".whyStatusMoneybox").html("소유 골드: "+ jiwoo.golds +"골드 "+"<br/> 민트: "+ jiwoo.mint +"개 <br/>몬스터볼: "+ jiwoo.pokeBall+ "개");
 	console.log(storeMsg);
 }
 
